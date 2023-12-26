86031d3dbc2e6c4554678e5a2ed5266bf376a6a4
paresiqbal <thecrowdel@gmail.com>
2023-12-26 22:08:06 +0700

    Package OS

--- error.go
+++ error.go
@@ -1,36 +1,36 @@
-package main
+// package main
 
-import (
-	"errors"
-	"fmt"
-)
+// import (
+// 	"errors"
+// 	"fmt"
+// )
 
-var (
-	ValidationError = errors.New("validation error")
-	NotFound        = errors.New("not found")
-)
+// var (
+// 	ValidationError = errors.New("validation error")
+// 	NotFound        = errors.New("not found")
+// )
 
-func getById(id string) error {
-	if id == "" {
-		return ValidationError
-	}
+// func getById(id string) error {
+// 	if id == "" {
+// 		return ValidationError
+// 	}
 
-	if id != "Pares" {
-		return NotFound
-	}
+// 	if id != "Pares" {
+// 		return NotFound
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func main() {
-	err := getById("Pares")
-	if err != nil {
-		if errors.Is(err, ValidationError) {
-			fmt.Println("Validation Error")
-		} else if errors.Is(err, NotFound) {
-			fmt.Println("Not Found")
-		} else {
-			fmt.Println("Unknown Error")
-		}
-	}
-}
+// func main() {
+// 	err := getById("Pares")
+// 	if err != nil {
+// 		if errors.Is(err, ValidationError) {
+// 			fmt.Println("Validation Error")
+// 		} else if errors.Is(err, NotFound) {
+// 			fmt.Println("Not Found")
+// 		} else {
+// 			fmt.Println("Unknown Error")
+// 		}
+// 	}
+// }
