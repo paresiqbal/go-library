95550abed6f4da8d248502f097eceae17220576c
paresiqbal <thecrowdel@gmail.com>
2024-01-01 22:13:44 +0700

    Flag

---
+++ flag.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var username *string = flag.String("name", "root", "data username")
+	var password *string = flag.String("pass", "root", "data password")
+	var host *string = flag.String("host", "localhost", "data host")
+	var port *int = flag.Int("port", 3306, "data port")
+
+	flag.Parse()
+
+	fmt.Println("username:", *username)
+	fmt.Println("password:", *password)
+	fmt.Println("host:", *host)
+	fmt.Println("port:", *port)
+}
