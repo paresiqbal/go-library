a7b104a746bc65fa28a6a60ea0306dbdd0c0a1aa
paresiqbal <thecrowdel@gmail.com>
2024-01-04 14:14:25 +0700

    Add everyting

--- string.go
+++ string.go
@@ -1,11 +1,15 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"strings"
-)
+// import (
+// 	"fmt"
+// 	"strings"
+// )
 
-func main() {
-	fmt.Println(strings.Contains("Hello, world!", "Hello"))
-	fmt.Println(strings.Split("Hai semuanya, Saya pares", " "))
-}
+// func main() {
+// 	fmt.Println(strings.Contains("Hello, world!", "Hello"))
+// 	fmt.Println(strings.Split("Hai semuanya", " "))
+// 	fmt.Println(strings.ToLower("ABCD"))
+// 	fmt.Println(strings.ToUpper("ABCD"))
+// 	fmt.Println(strings.TrimSpace("  Hello, world!  "))
+// 	fmt.Println(strings.ReplaceAll("Pares Iqbal, Pares Prastowo", "Pares", "Pahreza"))
+// }
