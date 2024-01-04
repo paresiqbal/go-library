e21a4c4bcb9f879b41495139a13a50fb4a281a21
paresiqbal <thecrowdel@gmail.com>
2024-01-04 19:53:04 +0700

    Parsing value

---
+++ strconv.go
@@ -0,0 +1,28 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"strconv"
+// )
+
+// func main() {
+// 	result, err := strconv.ParseBool("true")
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 	} else {
+// 		println(result)
+// 	}
+
+// 	resultInt, err := strconv.Atoi("123")
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 	} else {
+// 		println(resultInt)
+// 	}
+
+// 	binary := strconv.FormatInt(10, 2)
+// 	fmt.Println(binary)
+
+// 	var stringInt string = strconv.Itoa(9999)
+// 	fmt.Println(stringInt)
+// }
