3013ad7766715b48a277377f7f349855daa628e5
paresiqbal <thecrowdel@gmail.com>
2024-01-06 14:46:40 +0700

    Loop Link List

--- doubleLinkList.go
+++ doubleLinkList.go
@@ -1,22 +1,26 @@
-package main
+// package main
 
-import (
-	"container/list"
-	"fmt"
-)
+// import (
+// 	"container/list"
+// 	"fmt"
+// )
 
-func main() {
-	data := list.New()
-	data.PushBack("Pares")
-	data.PushBack("Iqbal")
-	data.PushBack("Prastowo")
+// func main() {
+// 	data := list.New()
+// 	data.PushBack("Pares")
+// 	data.PushBack("Iqbal")
+// 	data.PushBack("Prastowo")
 
-	head := data.Front()
-	fmt.Println(head.Value)
+// 	head := data.Front()
+// 	fmt.Println(head.Value)
 
-	next := head.Next()
-	fmt.Println(next.Value)
+// 	next := head.Next()
+// 	fmt.Println(next.Value)
 
-	next = next.Next()
-	fmt.Println(next.Value)
-}
+// 	next = next.Next()
+// 	fmt.Println(next.Value)
+
+// 	for i := data.Front(); i != nil; i.Next() {
+// 		fmt.Println(i.Value)
+// 	}
+// }
